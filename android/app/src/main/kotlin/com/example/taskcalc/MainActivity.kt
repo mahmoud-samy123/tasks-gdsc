2329b105d843d22076d7c17051937828de7de8b9
DESKTOP-06K69N1\pc <mahmtgdd522@gmail.com>
2024-02-24 23:43:13 +0200

    init project

---
+++ android/app/src/main/kotlin/com/example/taskcalc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.taskcalc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+// void main(){
+//
+//   print("Your Name IS: Mahmoud Samy");
+// }
+///////////////////////////////////
+// void main(){
+//
+//   print("Hello, I am John Doe.");
+// }
+
+////////////////////////////////
+// void main(){
+//
+//   int number=7;
+//   print("$number");
+// }
+//////////////////////////////////////
+
+// import 'dart:io';
+//
+// void main(){
+//   int num=int.parse(stdin.readLineSync()!);
+//   print("$num");}
+
+////////////////////////////////////
+// void main() {
+//   String num = "This is an example string with spaces.";
+//   String x = removeWhitespace(num);
+//   print("Original string: $num");
+//   print("String without whitespace: $x");
+// }
+//
+// String removeWhitespace(String input) {
+//   return input.replaceAll(RegExp(r'\s+'), '');
+// }
+
+/////////////////////////////////////////
+
+
+// import 'dart:developer';
+// import 'dart:io';
+//
+// void main() {
+//   print("Please enter number");
+//   int num=int.parse(stdin.readLineSync()!);
+//
+//   if (num%2==0){
+//     print (" $num :is even number");
+//
+//   }
+//   else if(num%2==1){
+//     print (" $num :is odd number");
+//
+//   }
+//   else{
+//     print("the number not found");
+//   }
+// }
+///////////////////////////////////
+// void main() {
+//   double distance = 25; // Distance in kilometers
+//   double speed = 40; // Speed in kilometers per hour
+//
+//   double timeInHours = calculateTime(distance, speed);
+//   int timeInMinutes = (timeInHours * 60).toInt();
+//
+//   print("Distance: $distance km");
+//   print("Speed: $speed km/h");
+//   print("Time taken: $timeInMinutes minutes");
+// }
+//
+// double calculateTime(double distance, double speed) {
+//   return distance / speed; // Time in hours
+// }
+
